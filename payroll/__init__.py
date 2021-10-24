0aeb14c83372012cada29ea46411c7deb865cbf3
removedporn <86824510+removedporn@users.noreply.github.com>
2021-10-24 13:09:18 +0800

    Delete payroll directory

--- payroll/__init__.py
+++
@@ -1,6 +0,0 @@
-# flake8: noqa
-from .company import Company
-from .employee import Employee
-from .payment import Payment
-from .payroll import Payroll
-from .timesheet import TimeSheet
--- payroll/company.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from decimal import Decimal
-
-
-@dataclass(frozen=True)
-class Company:
-    name: str
-    save_percentage: Decimal
-    overwork_rate: Decimal
